15ad1fbed68a68491ba24564d730d4593cec1d5b
Nicholas Le <nicholasl.le224@gmail.com>
2023-08-26 01:47:08 -0700

    Add Event Routes

--- server/routes/eventRouter.js
+++ server/routes/eventRouter.js
@@ -1,5 +1,62 @@
 const express = require("express");
+const User = require("../models/userModel");
+const Event = require("../models/eventModel");
+const mongoose = require("mongoose");
+
+require('dotenv').config({ path: '../config.env' });
 
 const eventRouter = express.Router();
 
-eventRouter.post
+eventRouter.post("/add-event", async (req, res) => {
+    const { user_id, event } = req.body;
+  
+    // Validate user input
+    if (!(user_id && event)) {
+        return res.status(400).json({
+            status: 400,
+            message: "Missing parameters"
+        });
+    }
+    
+    await User.updateOne( 
+        { 
+            _id: mongoose.Types.ObjectId(user_id) 
+        },
+        {
+            $push: 
+            { 
+                events: new Event(event)
+            }
+        }
+    );
+
+    return res.status(200).json(event._id);
+});
+
+eventRouter.post("/delete-event", async (req, res) => {
+    const { user_id, event_id } = req.body;
+
+    // Validate user input
+    if (!(user_id && event_id)) {
+        return res.status(400).json({
+            status: 400,
+            message: "Missing parameters"
+        });
+    }
+
+    await User.updateOne( 
+        { 
+            _id: mongoose.Types.ObjectId(user_id) 
+        },
+        { 
+            $pull: 
+            { 
+                events: { _id: mongoose.Types.ObjectId(event_id) } 
+            } 
+        }
+    );
+
+    return res.status(200).json(event_id);
+});
+
+module.exports = eventRouter;
